379355eddbe179ddd610689c0ac8a8224e15645b
Solomon <gbolahanbalogun5@gmail.com>
2020-02-17 00:00:32 +0100

    Delete repeated comments from discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -171,25 +171,6 @@ async def youtube(ctx, *, search):
     search_results = re.findall('href=\"\\/watch\\?v=(.{11})', htm_content.read().decode())
     await ctx.send('http://www.youtube.com/watch?v=' + search_results[0])
 
-######################## ZTM DISCORD BOT #########################
-# This project will start off simple and as we progress we can make it more complex with cogs(OOP)
-
-# import discord.py first
-
-# first we need to get the bot to join a discord server
-# after we connect to a discord server we will print all the users that join or leave the server
-# after this we will start with basic commands
-# the first command will be a simple reply from the bot example if we do !ping it should reply "pong"
-
-
-######################## Packages we will be starting with #########################
-# Discord.py for documentation refer to  https://discordpy.readthedocs.io/en/latest/
-
-
-######################## IMPORTAN NOTICES WHEN USING DISCORD.PY #########################
-
-# Discord.py was rewritten the latest version is known as (rewrite) due to this i recommend using python 3.7 and up to avoid errors/conflicts
-
 
 @client.command()
 async def todo(ctx, *args):
