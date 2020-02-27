cf1a230fa5b2f39b92e5cc6a8f95ca8b1a491881
Jeremy <jabeck@gmail.com>
2020-02-27 09:59:41 -0800

    started scraper code for hackerrank.com

--- cogs/challenges.py
+++ cogs/challenges.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+from hyper.contrib import HTTP20Adapter
 from discord.ext import commands
 
 
@@ -5,6 +8,31 @@ class Challenges(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.url = 'https://www.hackerrank.com/domains/python'
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
+        self.get_results()
+
+
+    def create_links(self, links, rank):
+        hn = []
+        for idx, item in enumerate(links):
+            title = item.getText()
+            href = item.get('href', None)
+            vote = rank[idx].select('.difficulty')
+            if len(vote):
+                r = int(vote[0].getText().replace(' points', ''))
+                if r == rank:
+                    hn.append({'title': title, 'link': href, 'votes': points})
+        return hn
+
+    def get_results(self):
+        conn = requests.Session()
+        conn.mount('https://', HTTP20Adapter())
+        resp = conn.get(self.url, headers=self.headers)
+        soup = BeautifulSoup(resp.content, 'html.parser')
+        links = soup.select('.challenge-list-item')
+        rank = soup.select('.difficulty')
+        print(enumerate(links))
 
     @commands.command(name='beginner', aliases=['Beginner'])
     async def beginner(self, ctx):
